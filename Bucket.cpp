0c6e697b2ec81c40f44676769b4d895b4de73118
mlapuste <mihai_lapuste@sfu.ca>
2020-06-22 16:29:03 -0700

    Update Bucket.cpp

--- Bucket.cpp
+++ Bucket.cpp
@@ -5,13 +5,13 @@
 
 using namespace std;
 
-Bucket::Bucket(int array_size){ // constructor
-    max_n_keys = array_size; // set the max array/bucket size.
+Bucket::Bucket(int max_array_size, int local_depth){ // constructor
+    max_n_keys = max_array_size; // set the max array/bucket size.
     key_array = new int[max_n_keys - 1]; // point key_array pointer to newly initialized integer array for bucket.
     for (int i=0; i<max_n_keys; i++) {
         key_array[i] = -1;    // Initialize all elements to -1.
     }
-    local_depth = 1;
+    l_depth = local_depth;
     n_keys = 0;
 }
 
@@ -20,12 +20,17 @@ Bucket::~Bucket(){ // destructor
     key_array = NULL;     // Be sure the deallocated memory isn't used.
 }
 
-void Bucket::insert(int key){
-    key_array[n_keys] = key;
-    if( ( n_keys + 1 ) > max_n_keys ){
-        //  cout << "bucket overflow: " << key << endl;
-    }
-    else n_keys++;
+bool Bucket::insert(int key){
+    if(n_keys < max_n_keys){
+        for(int i=0; i < max_n_keys; i++){
+            if(key_array[i] == -1){ // if empty spot in bucket found.
+                key_array[i] = key; // insert key in empty spot.
+                n_keys++;
+                return true;
+            }
+        }
+    } 
+    return false; // split bucket + create new + inc local depth & (or) increase directory size
 }
 
 bool Bucket::find(int key){
@@ -33,6 +38,19 @@ bool Bucket::find(int key){
          if(key == key_array[i]) return true;
      }
      return false; // if nothing is found, return false
+}
+
+bool Bucket::remove(int key){
+     if(find(key)){
+        for(int i=0; i < max_n_keys; i++){
+            if(key == key_array[i]){
+                key_array[i] = -1;
+                n_keys--;
+            }
+        }
+        return true;
+     } 
+     else return false; // if nothing is found, return false
 }
 
 void Bucket::print(){
@@ -44,5 +62,5 @@ void Bucket::print(){
 
         if(i < (max_n_keys-1)) {cout << ",";}
     }
-    cout << "] (" << local_depth << ')' << endl;
+    cout << "] (" << l_depth << ')' << endl;
 }
